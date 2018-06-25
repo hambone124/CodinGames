bd575941649addb32f52820d719daa0522c69192
Hamilton Perez <hambone124@users.noreply.github.com>
2018-06-24 21:03:13 -0400

    AsciiArt - init and GenerateCharArray method

---
+++ AsciiArt/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/* 
+4
+5 
+E
+ #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### 
+# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # 
+### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## 
+# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       
+# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  
+ */
+ 
+ /**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        string T = Console.ReadLine();
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+        }
+
+        
+
+        var charArray = GenerateCharArray();
+
+
+
+        Console.WriteLine("answer");
+    }
+
+    static char[] GenerateCharArray ()
+    {
+        int i = 0;
+        char[] charArray = new char[26];
+        for (char letter = 'A'; letter < 'Z'; letter++)
+        {
+            charArray[i] = letter;
+        }
+        return charArray;
+    }
+}
