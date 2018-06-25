830ba4dc2aad866bd225537eb6c63514f1689bf3
Hamilton Perez <hambone124@users.noreply.github.com>
2018-06-25 17:44:37 -0400

    AsciiArt - final answer

--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -5,50 +5,58 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/* 
-4
-5 
-E
- #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### 
-# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # 
-### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## 
-# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       
-# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  
- */
- 
- /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Solution
 {
     static void Main(string[] args)
     {
         int L = int.Parse(Console.ReadLine());
         int H = int.Parse(Console.ReadLine());
-        string T = Console.ReadLine();
+        string T = Console.ReadLine().ToUpper();
+        string[] displayArray = NewDisplayArray(H);
+        char[] alphabetArray = NewAlphabetArray();
+
+        string[] outputArray = ConvertStringToDisplay(T,displayArray,alphabetArray,L,H);
+
+        PrintAnswer(outputArray);
+    }
+
+    static string[] NewDisplayArray(int H)
+    {
+        string[] displayArray = new string[H];
         for (int i = 0; i < H; i++)
         {
-            string ROW = Console.ReadLine();
+            displayArray[i] = Console.ReadLine();
         }
-
-        
-
-        var charArray = GenerateCharArray();
-
-
-
-        Console.WriteLine("answer");
+        return displayArray;
     }
 
-    static char[] GenerateCharArray ()
+    static char[] NewAlphabetArray ()
     {
-        int i = 0;
-        char[] charArray = new char[26];
-        for (char letter = 'A'; letter < 'Z'; letter++)
-        {
-            charArray[i] = letter;
-        }
-        return charArray;
+        return ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").ToCharArray();
     }
-}
+
+    static string[] ConvertStringToDisplay (string input, string[] displayArray, char[] alphabetArray, int L, int H)
+    {
+        string[] output = new string[H];
+        foreach (char letter in input)
+        {
+            int letterIndex = Array.IndexOf(alphabetArray,letter);
+            if (letterIndex == -1) { letterIndex = 26; }
+            int scanPosition = L * letterIndex;
+            
+            for (int i = 0; i < H; i++)
+            {
+                output[i] += displayArray[i].Substring(scanPosition,L);
+            } 
+        }
+        return output;
+    }
+
+    static void PrintAnswer (string[] outputArray)
+    {
+        foreach (string displayRow in outputArray)
+        {
+            Console.WriteLine(displayRow);
+        }
+    }
+} 
